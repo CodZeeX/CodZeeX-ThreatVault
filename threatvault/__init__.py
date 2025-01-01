9209015d8db6b4851496fc867f8477fac4e09d18
RobsHs <fredapacitan@gmail.com>
2025-01-01 16:43:24 +0700

    chore(init): setup repository architecture and threatvault/__init__.py

---
+++ threatvault/__init__.py
@@ -0,0 +1,10 @@
+"""
+CodZeeX ThreatVault Framework
+Author: RobsHs <fredapacitan@gmail.com>
+Organization: CodZeeX (https://github.com/organizations/CodZeeX)
+License: MIT
+"""
+
+__version__ = "2.4.0"
+__author__ = "RobsHs"
+__organization__ = "CodZeeX"
